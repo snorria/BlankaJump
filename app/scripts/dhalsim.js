b94d69bb6a8459397b828ae37e2d6e47d8bd98a7
Snorri Arnarson <snorriarn@gmail.com>
2013-08-29 04:21:01 +0000

    Refactoring búið, bætti einnig við Dhalsim sem enemy.
    Breytti collision detection.
    Bætti við game over
    Bætti við freeze
    Breytti að maður hoppar endalaust.

--- app/scripts/dhalsim.js
+++ app/scripts/dhalsim.js
@@ -1,4 +1,20 @@
-var Dhalsim = function(el) {
-  this.el = el;
+define(function() { 
 
+var Dhalsim = function(pos,width,height) {
+  this.el = $('<div class="dhalsim"></div>');
+  this.pos = pos;
+  this.width = width;
+  this.height = height;
+  this.radius = width;
+  //this.rightX = this.pos.x+this.width;
+  this.el.css({
+    left: this.pos.x,
+    bottom: this.pos.y,
+    width: this.width,
+    height: this.height
+  });
 };
+
+  Dhalsim.prototype.onFrame = function() {};
+  return Dhalsim;
+});
